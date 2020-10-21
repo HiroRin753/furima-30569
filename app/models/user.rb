f46f96ba2ff556e0fd454a8d6c759b66d265684e
HiroRin753 <chikuzen.0306@gmail.com>
2020-10-21 10:45:29 +0900

    s

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  has_many :items
+  has_many :comments
+  has_many :purchases
+  
+  
+  validates :
 
 end
