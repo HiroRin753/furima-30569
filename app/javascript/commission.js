a8e9bdbf751f9b58fb3bf8bfd4d9bbcb65ce5cb0
HiroRin753 <chikuzen.0306@gmail.com>
2020-10-27 17:23:01 +0900

    JS実装

--- app/javascript/commission.js
+++ app/javascript/commission.js
@@ -1,8 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-   const priceInput = document.getElementById('item-price');
-   priceInput.addEventListener("input", () => {
-      console.log("イベント発火");
-   })
-
-
-
+document.addEventListener('DOMContentLoaded', 
+function commission () {
+   const itemPrice = document.getElementById("item-price");
+   itemPrice.addEventListener("keyup", () => {
+      const addTaxPrice = document.getElementById('add-tax-price');
+      const addTaxPriceVal = itemPrice.value * 0.1;
+      const Profit= document.getElementById('profit');
+      const ProfitVal= itemPrice.value - addTaxPriceVal;
+      addTaxPrice.innerHTML = `${Math.floor(addTaxPriceVal)}`;
+      Profit.innerHTML = `${Math.floor(ProfitVal)}`;
+   });
+});
--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :image
-    validates :price
+    validates :price, format: { with: /\A[0-9]{}+\z/}, numericality: { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }
     validates :description
 
     with_options numericality: { other_than: 1 } do
