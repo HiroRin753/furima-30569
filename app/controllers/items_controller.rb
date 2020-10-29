09f3b0e3fe1d19ee01865ce1cfa2d634cece250c
HiroRin753 <chikuzen.0306@gmail.com>
2020-10-29 12:06:43 +0900

    商品詳細表示実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,25 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+
+    category = Category.data.detect{|o| o[:id] == @item.category_id}
+    @category = category[:name]
+
+    condition = Condition.data.detect{|o| o[:id] == @item.condition_id}
+    @condition = condition[:name]
+
+    shippingcost = ShippingCost.data.detect{|o| o[:id] == @item.shipping_cost_id}
+    @shippingcost = shippingcost[:name]
+
+    shipfrom = ShipFrom.data.detect{|o| o[:id] == @item.ship_from_id}
+    @shipfrom = shipfrom[:name]
+
+    daystoship = DaysToShip.data.detect{|o| o[:id] == @item.days_to_ship_id}
+    @daystoship = daystoship[:name]
+  end
+
   private
   def items_params
     params.require(:item).permit(:name, :image, :description, :condition_id, :category_id, :shipping_cost_id, :days_to_ship_id, :ship_from_id, :price,)
